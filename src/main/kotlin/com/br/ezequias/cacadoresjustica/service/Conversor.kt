4ae8b16d6a140d4d8ddd43e753f3dba9b727f428
ezequiassam <ezequiassam@gmail.com>
2018-08-30 22:28:42 -0300

    mapeando objetos

--- src/main/kotlin/com/br/ezequias/cacadoresjustica/service/Conversor.kt
+++ src/main/kotlin/com/br/ezequias/cacadoresjustica/service/Conversor.kt
@@ -1,14 +1,12 @@
 package com.br.ezequias.cacadoresjustica.service
 
-import com.br.ezequias.cacadoresjustica.model.Processo
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
-import org.jsoup.nodes.TextNode
 import org.jsoup.select.Elements
 
 class Conversor {
 
-//    TODO CRIAR TRATAMENTO PARA FALHA DE CONECÇÃO E PROCESSO NÃO ENCONTRADO
+    //    TODO CRIAR TRATAMENTO PARA FALHA DE CONEXÃO E PROCESSO NÃO ENCONTRADO
     fun getPagina() {
         var url = "http://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=7J0001D040000&processo.foro=271&uuidCaptcha=sajcaptcha_279c1c241c254bb4a60825e4a39101c1"
         var doc = Jsoup.connect(url).get()
@@ -16,7 +14,6 @@ class Conversor {
         var dadosProcesso = getElementsDadosProcesso(elements)
         var partes = doc.getElementById("tableTodasPartes")
         var movimentacoes = doc.getElementById("tabelaTodasMovimentacoes")
-        println("Dados do processo: " + dadosProcesso?.text() + "\nPartes envolvidas: " + partes.text() + "\nMovimentacoes: " + movimentacoes.text())
         transformProcesso(dadosProcesso, partes, movimentacoes)
     }
 
@@ -30,13 +27,40 @@ class Conversor {
     }
 
     fun transformProcesso(dadosProcesso: Element?, partes: Element, movimentacoes: Element) {
-        var d = dadosProcesso?.children()?.get(0)?.children()
-        var map = mutableMapOf<String, String>()
-        for (t in d!!) {
-            map.put(t.text().substringBefore(":").trim().toUpperCase(), t.text().substringAfter(":").trim())
-        }
-        var p = partes.dataset()
-        var m = movimentacoes.dataset()
+        var dadosMap = mapDados(dadosProcesso?.children()?.get(0)?.children())
+        var partesList = listPartes(partes.children().get(0).children())
+        var movimentacoesList = listMovimentacoes(movimentacoes.children())
     }
+
+    fun mapDados(elements: Elements?): MutableMap<String, String> {
+        var map = mutableMapOf<String, String>()
+        for (e in elements!!) {
+            var key = e.text().substringBefore(":").trim().toUpperCase()
+            var value = e.text().substringAfter(":").trim()
+            map.put(key, value)
+        }
+        return map
+    }
+
+    fun listPartes(elements: Elements?): List<Map<String, String>> {
+        var list = mutableListOf<Map<String, String>>()
+        for (e in elements!!) {
+            var key = e.text().substringBefore(":").trim().toUpperCase()
+            var value = e.text().substringAfter(":").trim()
+            list.add(mapOf(key to value))
+        }
+        return list
+    }
+
+    fun listMovimentacoes(elements: Elements?): List<Map<String, String>> {
+        var list = mutableListOf<Map<String, String>>()
+        for (e in elements!!) {
+            var key = e.text().trim().substring(0, 10).toUpperCase()
+            var value = e.text().substring(11).trim()
+            list.add(mapOf(key to value))
+        }
+        return list
+    }
+
 
 }
